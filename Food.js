c0f920254e114d8a68d9a53ef71f7606a5d70caa
HaarisQadri <72584587+HaarisQadri@users.noreply.github.com>
2020-11-30 19:28:44 -0800

    Update Food.js

--- Food.js
+++ Food.js
@@ -43,13 +43,15 @@ class Food{
                 this.lastFed = lastFed
             }
     bedroom(){
-        
+        background(bedroom, 550, 500)
     }
     
     garden(){
+        background(garden, 550, 500)
     }
     
     washroom(){
+        background(washroom, 550, 500)
     }
     
 }
